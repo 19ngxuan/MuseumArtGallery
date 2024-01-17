577c61d23e833fe00b710efec223dd7f271fd502
Khanh.XuanNguyen <19ngxuan@gmail.com>
2024-01-17 10:04:33 +0100

    deleting unused imports

--- app/src/main/java/com/example/improvedmuseumartgallery/presentation/screens/favoriteDetail/FavoriteDetailViewModel.kt
+++ app/src/main/java/com/example/improvedmuseumartgallery/presentation/screens/favoriteDetail/FavoriteDetailViewModel.kt
@@ -15,29 +15,6 @@ class FavoriteDetailViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
-//    //TODO convert to reactive approach
-//    //TODO it should be state flow.
-//    private val _favoriteDetailUiState = MutableStateFlow<UiState<FavoriteArtwork>>(UiState.Loading)
-//    val favoriteDetailUiState: StateFlow<UiState<FavoriteArtwork>> =
-//        _favoriteDetailUiState.asStateFlow()
-//
-//    //TODO after reactive approach we don't need it anymore, remove init block
-//    init {
-//        val artIdString = savedStateHandle.get<String>("artId")
-//        val artId = artIdString?.toIntOrNull()
-//        artId?.let { id ->
-//            viewModelScope.launch {
-//                getFavoriteArtworkByIdUseCase(id).collect { result ->
-//                    if (result != null) {
-//                        _favoriteDetailUiState.value = UiState.Success(result)
-//                    }
-//                }
-//            }
-//        } ?: run {
-//            _favoriteDetailUiState.value = UiState.Error
-//        }
-//    }
-
 
     @OptIn(ExperimentalCoroutinesApi::class)
     val favoriteArtwork = savedStateHandle.getStateFlow<String?>("artId", null)
